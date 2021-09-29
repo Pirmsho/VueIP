47fcf2f4515f2f04ebf72edbe09d329abcbc27c4
Pirmsho <d.pirmisashvili@gmail.com>
2021-09-29 13:43:45 +0200

    init IP proj

---
+++ src/components/IPinfo.vue
@@ -0,0 +1,45 @@
+<template>
+  <div
+    class="
+      absolute
+      -bottom-64
+      lg:-bottom-20
+      flex flex-col
+      lg:flex-row
+      bg-white
+      gap-y-4 gap-x-12
+      px-8
+      pt-8
+      pb-12
+      rounded-md
+    "
+  >
+    <div class="flex flex-col">
+      <h3 class="text-xs mb-2 uppercase">IP Address</h3>
+      <span class="text-xl">{{ IpInfo.address }}</span>
+    </div>
+    <div class="flex flex-col">
+      <h3 class="text-xs mb-2 uppercase">Location</h3>
+      <span class="text-xl">{{ IpInfo.state }}</span>
+    </div>
+    <div class="flex flex-col">
+      <h3 class="text-xs mb-2 uppercase">City</h3>
+      <span class="text-xl">{{ IpInfo.city }}</span>
+    </div>
+    <div class="flex flex-col">
+      <h3 class="text-xs mb-2 uppercase">Timezone</h3>
+      <span class="text-xl">UTC {{ IpInfo.timezone }}</span>
+    </div>
+    <div class="flex flex-col">
+      <h3 class="text-xs mb-2 uppercase">ISP</h3>
+      <span class="text-xl">{{ IpInfo.isp }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["IpInfo"],
+};
+</script>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,110 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="flex flex-col h-screen max-h-screen">
+    <div
+      class="
+        z-20
+        flex
+        justify-center
+        relative
+        bg-hero-pattern bg-cover
+        px-4
+        pt-8
+        pb-32
+      "
+    >
+      <div class="w-full max-w-screen-sm">
+        <h1 class="text-white text-center text-3xl pb-4">IP Address Tracker</h1>
+        <div class="flex">
+          <input
+            v-model="queryIP"
+            type="text"
+            placeholder="Search for any IP or just leave empty to get your location"
+            class="
+              flex-1
+              py-3
+              px-2
+              rounded-tl-md rounded-bl-md
+              focus:outline-none
+            "
+          />
+          <i
+            @click="getIpInfo"
+            class="
+              fa fa-chevron-right
+              cursor-pointer
+              bg-black
+              text-white
+              px-4
+              rounded-tr-md rounded-br-md
+              flex
+              items-center
+            "
+          ></i>
+        </div>
+      </div>
+
+      <IPinfo v-if="IpInfo" :IpInfo="IpInfo" />
+    </div>
+    <div id="mapid" class="h-full z-10"></div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import IPinfo from "../components/IPinfo.vue";
+import leaflet from "leaflet";
+import { onMounted, ref } from "@vue/runtime-core";
+import axios from "axios";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: { IPinfo },
+  setup() {
+    let mymap;
+    const queryIP = ref("");
+    const IpInfo = ref(null);
+    onMounted(() => {
+      mymap = leaflet.map("mapid").setView([51.505, -0.09], 9);
+
+      leaflet
+        .tileLayer(
+          "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoibm9vZ2FpIiwiYSI6ImNrdTVkcmNlYTE3enEydm82cDY0c3pyd3cifQ.dh2Vu3NJyfyJlMssk0Gopw",
+          {
+            attribution:
+              'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: "mapbox/streets-v11",
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken:
+              "pk.eyJ1Ijoibm9vZ2FpIiwiYSI6ImNrdTVkcmNlYTE3enEydm82cDY0c3pyd3cifQ.dh2Vu3NJyfyJlMssk0Gopw",
+          }
+        )
+        .addTo(mymap);
+    });
+
+    const getIpInfo = async () => {
+      try {
+        const data = await axios.get(
+          `https://geo.ipify.org/api/v1?apiKey=at_E2UYLaaMGUUvYZ8fQ8Inyaa56g1X0&ipAddress=${queryIP.value}`
+        );
+        const result = data.data;
+        console.log(result);
+        IpInfo.value = {
+          address: result.ip,
+          state: result.location.region,
+          city: result.location.city,
+          timezone: result.location.timezone,
+          isp: result.isp,
+          lat: result.location.lat,
+          lng: result.location.lng,
+        };
+        leaflet.marker([IpInfo.value.lat, IpInfo.value.lng]).addTo(mymap);
+        mymap.setView([IpInfo.value.lat, IpInfo.value.lng], 13);
+      } catch (err) {
+        alert(err.message);
+      }
+    };
+
+    return { queryIP, IpInfo, getIpInfo };
+  },
+};
 </script>
